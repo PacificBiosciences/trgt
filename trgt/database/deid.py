8630d31693608e8db2a5c6e66268e07c7c99a58f
Adam English <ACEnglish@gmail.com>
2023-05-05 01:26:00 -0400

    optional sample shuffle in deid

--- trgt/database/deid.py
+++ trgt/database/deid.py
@@ -6,8 +6,10 @@ import sys
 import shutil
 import logging
 import argparse
-import pandas as pd
 import truvari
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
 import trgt
 
 def check_args(args):
@@ -43,6 +45,8 @@ def deid_main(args):
                         help="Output DB")
     parser.add_argument("-s", "--remove-sequences", action="store_true",
                         help="Remove sequences from allele table")
+    parser.add_argument("-S", "--shuffle-samples", action="store_true",
+                        help="Shuffle sample tables together and split (experimental)")
     args = parser.parse_args(args)
     truvari.setup_logging()
 
@@ -53,14 +57,48 @@ def deid_main(args):
     in_file_names = trgt.get_tdb_filenames(args.input)
     out_file_names = trgt.get_tdb_filenames(args.output)
     os.mkdir(args.output)
+    logging.info("Copying locus")
     shutil.copy(in_file_names['locus'], out_file_names['locus'])
 
     alleles = pd.read_parquet(in_file_names['allele'])
     if args.remove_sequences:
         alleles["sequence"] = ""
+    logging.info("Changing allele_numbers")
     ref = alleles[alleles["allele_number"] == 0].copy()
     alt = alleles[alleles["allele_number"] != 0].copy().sort_values(["LocusID", "allele_length"])
     alt["allele_number"] = alt.groupby(["LocusID"]).cumcount() + 1
     out = pd.concat([ref, alt]).sort_values(["LocusID", "allele_number"])
     out.to_parquet(out_file_names['allele'], index=False, compression='gzip')
+
+    s_schema = pa.schema([('LocusID', pa.uint32()),
+                          ('allele_number', pa.uint16()),
+                          ('spanning_reads', pa.uint16()),
+                          ('length_range_lower', pa.uint16()),
+                          ('length_range_upper', pa.uint16()),
+                          ('average_methylation', pa.uint16())
+                        ])
+
+    if args.shuffle_samples:
+        logging.info("Shuffling samples (experimental)")
+        parts = []
+        n_samps = 0
+        for i in in_file_names['sample'].values():
+            parts.append(pd.read_parquet(i))
+            n_samps += 1
+        samps = pd.concat(parts).sample(frac=1).sort_values(["LocusID"])
+        for idx in range(n_samps):
+            o_fn = os.path.join(args.output, f"sample.{idx}.pq")
+            value = samps.iloc[idx:len(samps):n_samps]
+            # Awful code duplication. Need to separate out dump_tdb
+            m_table = pa.Table.from_pandas(value)
+            n_table = []
+            n_names = []
+            for col, dtype in zip(s_schema.names, s_schema.types):
+                n_table.append(pa.compute.cast(m_table[col], dtype))
+                n_names.append(col)
+            n_table = pa.Table.from_arrays(n_table, names=n_names)
+            writer = pq.ParquetWriter(o_fn, s_schema, compression='gzip')
+            writer.write_table(n_table)
+            writer.close()
+
     logging.info("Finished")
